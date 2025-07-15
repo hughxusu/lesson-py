19324d1815c3da8bfd6df4fdbd20df27422d9305
hughxusu <hughxusu@qq.com>
2025-07-15 16:15:52 +0800

    完成基本知识

---
+++ codes/09-数据类型与程序流程/a-最大最小值.py
@@ -0,0 +1,17 @@
+message = 'hello, world'
+print(max(message))
+print(min(message))
+
+nums = [10, 20, 30, 40]
+print(max(nums))
+print(min(nums))
+
+nums = set(nums)
+print(max(nums))
+print(min(nums))
+
+letters = {'a': 1, 'b': 2, 'c': 3}
+print(max(letters))
+print(max(letters.values()))
+print(min(letters))
+print(min(letters.values()))
