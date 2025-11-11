641f23af527aa85efe9330dbc031f5df342c12df
hughxusu <hughxusu@qq.com>
2025-11-11 18:51:36 +0800

    重构前两章代码

---
+++ codes/02-简单数据类型/a-1-数值类型.py
@@ -0,0 +1,5 @@
+# 定义数值型变量
+month = 6
+pi = 3.14
+print(month)
+print(pi)
--- codes/02-简单数据类型/a-数值类型.py
+++
@@ -1,8 +0,0 @@
-# 定义数值型变量
-month = 6
-pi = 3.14
-print(month)
-print(pi)
-
-universe_age = 14_000_000_000
-print(universe_age)
