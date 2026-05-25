7173efd611f368805f399da5e087d8f490b28113
hughxusu <hughxusu@qq.com>
2026-05-25 19:02:53 +0800

    增加广告页面

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,16 @@
 // .vitepress/theme/index.ts
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import MyAdComponent from './MyAdComponent.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme, // 继承 VitePress 的默认主题
   /* 如果后续有需要，你还可以在这里注册全局组件或做其他扩展 */
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      // 使用 aside-ads-after 插槽，将其注入到右侧栏广告位
+      'aside-ads-after': () => h(MyAdComponent)
+    })
+  }
 }
