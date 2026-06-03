8909a629189487d1f1909e863f649ce54476a64e
hughxusu <hughxusu@qq.com>
2026-06-03 17:26:40 +0800

    完成列表章节的内容

--- codes/06-列表与元组/g-4-列表赋值.py
+++
@@ -1,5 +0,0 @@
-colors = ['red', 'green', 'blue', 'yellow', 'white', 'black']
-short_colors = ['purple', 'pink']
-print(colors)
-colors += short_colors
-print(colors)
--- codes/06-列表与元组/k-1-元组拼接.py
+++
@@ -1,8 +0,0 @@
-sites = ('Google', 'Runoob', 'Wiki', 'Taobao', 'Wiki', 'Weibo', 'Weixin')
-short_sites = ('Tiktok', 'Facebook')
-print(sites)
-print(short_sites)
-all_sites = sites + short_sites
-print(all_sites)
-print(type(all_sites))
-
