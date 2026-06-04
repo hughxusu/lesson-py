df91b8d1c8f7ed7f72a9873c293d8a3efa8b2bdf
hughxusu <hughxusu@qq.com>
2026-06-04 13:21:42 +0800

    完成函数章节

--- codes/10-认识函数/b-定义函数.py
+++ codes/10-认识函数/b-定义函数.py
@@ -7,8 +7,8 @@ def get_average(arr):
 
 a = [6.5, 7, 8, 7]
 avg = get_average(a)
-print(f'平均值是: {avg}')
+print(f'a的平均值是: {avg}')
 
-# b = [9.5, 7.5, 8, 8, 7]
-# avg = get_average(b)
-# print(f'平均值是: {avg}')
+b = [9.5, 7.5, 8, 8, 7]
+avg = get_average(b)
+print(f'b的平均值是: {avg}')
