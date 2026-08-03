003abe940acaf3a066b3b971db90283b1d5ceb08
hughxusu <hughxusu@qq.com>
2026-08-03 10:49:43 +0800

    修改迭代器与生成器的代码细节

--- codes/22-生成器与迭代器/e-生成器应用.py
+++ codes/22-生成器与迭代器/e-生成器应用.py
@@ -1,13 +1,14 @@
 def fibonacci(num):
     a = 0
     b = 1
-    
+
     current_index = 0
-    while current_index < num:
-        result = a
+    while True:
+        yield a
         a, b = b, a + b
         current_index += 1
-        yield result
+        if current_index >= num:
+            break
 
 fib = fibonacci(10)
 result = []
